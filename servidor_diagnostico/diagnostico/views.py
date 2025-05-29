b3c7badabdd662a6aa13af09c63f2986fa2315d3
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-05-29 01:03:46 -0500

    cc

--- servidor_diagnostico/diagnostico/views.py
+++ servidor_diagnostico/diagnostico/views.py
@@ -1,14 +1,22 @@
+# diagnostico/views.py
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+
 from .models import Diagnostico
 from .serializers import DiagnosticoSerializer
 
+@method_decorator(csrf_exempt, name="dispatch")
 class DiagnosticoViewSet(viewsets.ModelViewSet):
+    """
+    Ahora este ViewSet ya no exigirá token CSRF en peticiones POST/PUT/DELETE.
+    """
     queryset = Diagnostico.objects.all()
     serializer_class = DiagnosticoSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        # si deseas filtrar por paciente autenticado
-        user = self.request.user
-        return Diagnostico.objects.filter(paciente=user)
+        # si quieres que cada usuario sólo vea sus propios diagnósticos:
+        return Diagnostico.objects.filter(paciente=self.request.user)
