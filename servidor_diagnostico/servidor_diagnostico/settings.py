d30a3163b901c990ffb7fcb2797da8531e9eea66
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-05-29 00:55:29 -0500

    cc

--- servidor_diagnostico/servidor_diagnostico/settings.py
+++ servidor_diagnostico/servidor_diagnostico/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "diagnostico",  # Your app for handling diagnostics
+    "diagnostico",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = "servidor_diagnostico.urls"
 
