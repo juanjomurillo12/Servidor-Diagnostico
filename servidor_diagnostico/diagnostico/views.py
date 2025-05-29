9f0274b7c30288260d9c93bf1e0a7b766f6d4a90
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-05-29 01:13:23 -0500

    cc

--- servidor_diagnostico/diagnostico/views.py
+++ servidor_diagnostico/diagnostico/views.py
@@ -1,17 +1,24 @@
 # diagnostico/views.py
+
 from rest_framework import viewsets
 from rest_framework.permissions    import AllowAny
 from rest_framework.authentication import SessionAuthentication
+from rest_framework.parsers       import JSONParser
 from .models       import Diagnostico
 from .serializers  import DiagnosticoSerializer
 
 class CsrfExemptSessionAuthentication(SessionAuthentication):
     def enforce_csrf(self, request):
-        # skip CSRF check
-        return
+        return  # desactiva CSRF
 
 class DiagnosticoViewSet(viewsets.ModelViewSet):
-    queryset             = Diagnostico.objects.all()
-    serializer_class     = DiagnosticoSerializer
-    permission_classes   = [AllowAny]
+    queryset               = Diagnostico.objects.all()
+    serializer_class       = DiagnosticoSerializer
+    permission_classes     = [AllowAny]
     authentication_classes = [CsrfExemptSessionAuthentication]
+    parser_classes         = [JSONParser]
+
+    def create(self, request, *args, **kwargs):
+        # imprime en consola lo que llega
+        print("=== REQUEST.DATA ===", request.data)
+        return super().create(request, *args, **kwargs)
