440b30ab0f9db5567c031e954efac3cdb8824ac3
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-05-29 01:07:38 -0500

    l

--- servidor_diagnostico/diagnostico/serializers.py
+++ servidor_diagnostico/diagnostico/serializers.py
@@ -1,7 +1,8 @@
+# diagnostico/serializers.py
 from rest_framework import serializers
 from .models import Diagnostico
 
 class DiagnosticoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Diagnostico
-        fields = ['id', 'paciente', 'comentarios', 'cirugia', 'creado_en']
+        fields = ['id', 'paciente', 'comentarios', 'cirugia']
